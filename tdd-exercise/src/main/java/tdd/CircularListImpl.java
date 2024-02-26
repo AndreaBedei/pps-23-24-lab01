6980e50873a8766359129e9787f25b75fa0e2997
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-02-26 11:24:12 +0100

    Create tests and implementation of size and isEmpty

---
+++ tdd-exercise/src/main/java/tdd/CircularListImpl.java
@@ -0,0 +1,44 @@
+package tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    final List<String> list = new ArrayList<>();
+
+    @Override
+    public void add(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public int size() {
+        return this.list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'previous'");
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+    }
+}
--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import tdd.CircularList;
+import tdd.CircularListImpl;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        circularList = new CircularListImpl();
+    }
+
+    @Test
+    void checkListEmpty(){
+        assertTrue(circularList.isEmpty());
+    }
+
+    @Test
+    void checkSizeList(){
+        assertEquals(circularList.size(), 0);
     }
 
 }
